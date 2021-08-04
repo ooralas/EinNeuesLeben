d27dbd2fde060360c0e1cbea9d034ec7c7147fa0
Salar <salar.alali@study.hs-duesseldorf.de>
2021-08-04 14:47:46 +0200

    next button mit animation vom JQuery

--- Assets/js/bootsfahrt.js
+++ Assets/js/bootsfahrt.js
@@ -4,11 +4,27 @@ const BACKGROUND = document.getElementById("backgroundIMG");
 
    
 
+$(document).ready(function(){
+    $("button").click(function(){
+        $("img").animate({
+            right: 20 + "px",
+            opacity: 1,
+            
+        },900);
+    });
+});
 
 window.addEventListener('scroll',function(){
     
     var value = window.scrollY;
     BOOT.style.left = value * 0.3  + 'px';
     BACKGROUND.style.left = -value * 0.17 + 'px';
+
+    console.log(value)
+    if(value > 3476){
+        $("button").trigger("click");
+        console.log("Hier wird gefeuert")
+        
+    }
     
 });
