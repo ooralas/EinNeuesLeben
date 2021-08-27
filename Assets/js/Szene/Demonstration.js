be336d5b4e5c89676a3425c2f4a33b9cb74a4d1e
Salar <salar.alali@study.hs-duesseldorf.de>
2021-08-27 16:53:17 +0200

    Die Objekte bewegen sich hin her

--- Assets/js/Szene/Demonstration.js
+++ Assets/js/Szene/Demonstration.js
@@ -5,9 +5,18 @@ window.addEventListener('scroll',function(){
     
     let value = window.scrollY;
    
-    CROWD.style.left = value * 0.3 + 192 + 'px';
-    SOLDIERS.style.left = -value * 0.17 + 384 + 'px';
+    if(value < 485){
+        CROWD.style.left = value * 0.3 + 192 + 'px';
+        SOLDIERS.style.left = -value * 0.17 + 384 + 'px';
+    }
+    if(value > 485){
+        CROWD.style.left = -value * 0.3 + 483 + 'px';
+        SOLDIERS.style.left = value * 0.17 + 219.1 + 'px';
+    }    
+    if(value > 1117){
+        CROWD.style.left = value * 0.3 - 187 + 'px';
+        SOLDIERS.style.left = -value * 0.17 + 599 + 'px';
+    }
     
-    console.log("left",parseInt(CROWD.style.left))
     console.log(value)
 });
