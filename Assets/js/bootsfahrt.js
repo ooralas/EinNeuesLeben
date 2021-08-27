b35ab297609812e1978aee3f8048b1f63c2f7ede
Salar <salar.alali@study.hs-duesseldorf.de>
2021-08-27 12:15:47 +0200

    Audio von der Szene wir abgespilet, wenn der User scrollt

--- Assets/js/bootsfahrt.js
+++ Assets/js/bootsfahrt.js
@@ -1,5 +1,9 @@
 const BOOT = document.getElementById("bootIMG");
 const BACKGROUND = document.getElementById("backgroundIMG");
+var sound = new Howl({
+    src: ['https://www.mboxdrive.com/Bootsfahrt_szene.mp3'],
+    html5:true
+  }); 
 
 
    
@@ -19,6 +23,7 @@ window.addEventListener('scroll',function(){
     var value = window.scrollY;
     BOOT.style.left = value * 0.3  + 'px';
     BACKGROUND.style.left = -value * 0.17 + 'px';
+    startSound();
 
     console.log(value)
     if(value > 3476){
@@ -26,3 +31,13 @@ window.addEventListener('scroll',function(){
         console.log("Hier wird gefeuert");
     }
 });
+
+let startSound = () => {
+    if(!sound.playing()){
+        sound.fade(0, 0.4, 5000);
+        sound.play();
+        
+    }else{
+        console.log("Sound is: ", sound.playing());
+    }
+}
