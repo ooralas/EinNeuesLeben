7c630e072b0e5d76323e3a6613e7f49a81322c16
Your Name <you@example.com>
2021-09-23 23:41:16 +0200

    bahnhof update 3 (funktionierende version)

--- Assets/js/Szene/Bahnhof.js
+++ Assets/js/Szene/Bahnhof.js
@@ -6,16 +6,21 @@ window.addEventListener('scroll',function(){
     
     let value = window.scrollY;
 
-    if(value<800){
-    ZUG_VORNE.style.left = 15  - value * 0.05  + '%';
-    //ZUG_VORNE.style.top = 25 + value * 0.00297 + '%';
-    ZUG_VORNE.style.width = 70 + value * 0.037 + '%';
-
-    ZUG_HINTEN.style.left = 5  + value * 0.05  + '%';
-    //ZUG_VORNE.style.top = 25 + value * 0.00297 + '%';
-    //ZUG_HINTEN.style.width = 70 + value * 0.037 + '%';
-
-    //DIALLO.style.left = 600 + -value * 0.3 + 'px';
+    if(value<600){
+    ZUG_VORNE.style.left = 64  - value * 0.05  + '%';
+    ZUG_VORNE.style.top = 40 - value * 0.027 + '%';
+    ZUG_VORNE.style.width = 15 + value * 0.037 + '%';
     }
+    if(value<500){
+    ZUG_HINTEN.style.left = -10+ value * 0.05  + '%';
+    ZUG_HINTEN.style.top = 21 + value * 0.015 + '%';
+    ZUG_HINTEN.style.width = 70 + -value * 0.037 + '%';
+
+    
+    }
+
+    DIALLO.style.width = 70 + value * 0.01 + '%';
+    DIALLO.style.top = 25 + -value * 0.007 + '%';
+    DIALLO.style.left = 15 + -value * 0.007 + '%';
     console.log(value)
 });
