d053248d2259dda5d652263de1003da40f80be33
Your Name <you@example.com>
2021-09-27 20:13:11 +0200

    update Trennung

---
+++ Assets/js/Szene/Trennung.js
@@ -0,0 +1,15 @@
+const ELTERN = document.getElementById("eltern");
+const DIALLO = document.getElementById("diallo");
+
+window.addEventListener('scroll',function(){
+    
+    let value = window.scrollY;
+
+    //DIALLO.style.height = 20 + value * 0.3 + '%';
+    //DIALLO.style.transform = "scale(2,2)"
+    DIALLO.style.width = 60 + value * 0.013 + '%';
+    DIALLO.style.left = 20 + -value * 0.013 + '%';
+    
+    
+    console.log(value)
+});
