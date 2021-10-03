0b824225f275b8c47a57d73d1fdaa690ffbf9b03
Salar <salar.alali@study.hs-duesseldorf.de>
2021-10-03 23:00:13 +0200

    Wohnsituation-Szene: timeBar ist da

--- Assets/js/Szene/Wohnsituation.js
+++ Assets/js/Szene/Wohnsituation.js
@@ -1,3 +1,6 @@
+const PROGRESSIVEBAR = document.getElementById("progBar");
+
+
 var sound = new Howl({
     src: ['https://www.mboxdrive.com/Wohnsituation.mp3'],
     html5:true
@@ -18,6 +21,7 @@ window.addEventListener('scroll',function(){
     
     let value = window.scrollY;
     startSound();
+    startTimeBar(sound.duration());
 
     console.log(value)
 
@@ -42,3 +46,5 @@ let startSound = () => {
         console.log("Sound is: ", sound.playing());
     }
 }
+
+let startTimeBar = (soundDuration) => PROGRESSIVEBAR.style.animation = "fillTimeBar " + soundDuration + "s" + " linear forwards";
