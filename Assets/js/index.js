397dc33b00d1e54955285516d700613f13c27f69
ooralas <63547913+ooralas@users.noreply.github.com>
2021-11-04 20:12:43 +0100

    weitere Logik, um zu wissen, dass der User zum ersten Mal in der Seite ist, um den Hinweis zu zeigen

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,14 +2,29 @@ const modal = document.getElementById("modal")
 const overlay = document.getElementById("overlay")
 const button = document.getElementById("modal__btn")
 
-window.onload = () => {
-    if(history.length < 2) {
-        modal.classList.add("active")
-        overlay.classList.add("active")
+
+let isPageReloaded = () => {
+    if (sessionStorage.getItem('pageHasBeenLoaded')) {
+        console.log('this is a reload');
+
+        return true;
     }
+    sessionStorage.setItem('pageHasBeenLoaded', 'true');
+    return false;
 }
 
-button.onclick = () => {
-    modal.classList.remove("active")
-    overlay.classList.remove("active")
+
+if(!isPageReloaded()) {
+    window.onload = () => {
+        // if(history.length < 2) {
+            modal.classList.add("active")
+            overlay.classList.add("active")
+        // }
+    }
+    
+    button.onclick = () => {
+        modal.classList.remove("active")
+        overlay.classList.remove("active")
+    } 
 }
+
