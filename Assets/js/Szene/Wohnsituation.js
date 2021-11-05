7c9227e61821c0cc801cb8393e584e91b0875d16
ooralas <63547913+ooralas@users.noreply.github.com>
2021-11-05 14:24:00 +0100

    Wohnsituation: unnötige Zeilen entfernen

--- Assets/js/Szene/Wohnsituation.js
+++ Assets/js/Szene/Wohnsituation.js
@@ -1,13 +1,11 @@
 const PROGRESSIVEBAR = document.getElementById("progBar");
 
-
 var sound = new Howl({
     src: ['https://www.mboxdrive.com/Wohnsituation.mp3'],
     html5:true
 });
 
 $(document).ready(function(){
-
     $("#nextBt").fadeIn(3000);
     $("#text").fadeIn(3000);
     $("#backBt").fadeIn(3000);
@@ -16,31 +14,14 @@ $(document).ready(function(){
 
 window.addEventListener('scroll',function(){
     
-    let value = window.scrollY;
     startSound();
     startTimeBar(sound.duration());
-
-    console.log(value)
-
-    if(value > 100){
-       // $("#text").fadeIn(1000);
-    }
-
-    if(value > 400){
-       // $("#nextBtTrigger").trigger("click");
-        console.log("Hier wird gefeuert");
-    }
-    
-    console.log(value);
-    
 });
 
 let startSound = () => {
     if(!sound.playing()){
         sound.fade(0, 0.4, 5000);
         sound.play();
-    }else{
-        console.log("Sound is: ", sound.playing());
     }
 }
 
