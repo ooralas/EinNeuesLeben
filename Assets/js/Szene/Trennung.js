845a457b0e7b1b084dfea0e7e834f68fbcdc2077
ooralas <63547913+ooralas@users.noreply.github.com>
2021-11-05 14:23:23 +0100

    Trennung: unnötige Zeilen entfernen

--- Assets/js/Szene/Trennung.js
+++ Assets/js/Szene/Trennung.js
@@ -3,53 +3,35 @@ const DIALLO = document.getElementById("diallo");
 const BACKGROUND = document.getElementById("background");
 const PROGRESSIVEBAR = document.getElementById("progBar");
 
-
 var sound = new Howl({
     src: ['https://www.mboxdrive.com/Trennung.mp3.mp3'],
     html5:true
 });
 
 $(document).ready(function(){
-
     $("#nextBt").fadeIn(3000);
     $("#text").fadeIn(3000);
     $("#backBt").fadeIn(3000);
-    
 });
 
 window.addEventListener('scroll',function(){
-    
-
     $("#scrollGif").fadeOut(1500);
     let value = window.scrollY;
 
-    
     DIALLO.style.width = 60 + value * 0.011 + '%';
     DIALLO.style.left = 20 -value * 0.011 + '%';
 
     ELTERN.style.width = 60 - value * 0.0011 + '%';
     ELTERN.style.right =  20 - value * 0.0011 + '%';
-    
-    console.log(value);
+
     startSound();
     startTimeBar(sound.duration());
-
-    if(value > 500){
-        //$("#text").fadeIn(1000);
-    }
-
-    if(value > 1000){
-        //$("#nextBtTrigger").trigger("click");
-        //console.log("Hier wird gefeuert");
-    }
 });
 
 let startSound = () => {
     if(!sound.playing()){
         sound.fade(0, 0.4, 5000);
         sound.play();
-    }else{
-        console.log("Sound is: ", sound.playing());
     }
 }
 
